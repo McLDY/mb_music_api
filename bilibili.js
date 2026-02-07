b9d6c28a7be060e7181a34bc88661d141e1fc510
McLDY <mcldy00@outlook.com>
2026-02-07 14:44:16 +0800

    1

--- bilibili.js
+++ bilibili.js
@@ -1,23 +1,40 @@
-const core = require("./bilibili.core.js"); // 你原来的插件
+const core = require("./bilibili.core.js");
 
 module.exports = {
     async search(keyword, page) {
+        // 使用 core.searchAlbum，因为 core.search 是按类型分发的
         const res = await core.search(keyword, page, "music");
+
         return {
             isEnd: res.isEnd,
             data: res.data.map(item => ({
-                id: item.id || item.cid || item.aid || item.bvid,
+                id: item.bvid || item.aid || item.cid || item.id,
                 title: item.title,
                 artist: item.artist,
                 album: item.album,
                 artwork: item.artwork,
-                duration: item.duration
+                duration: item.duration,
+                source: "bilibili"
             }))
         };
     },
 
-    async getSong(id, quality) {
-        const musicItem = await core.getMusicInfoById(id); // 你需要自己写这个
+    async getSong(id, quality = "standard") {
+        // 先获取视频信息（cid）
+        const cidRes = await core.getCid(id, null);
+        const data = cidRes.data;
+
+        const musicItem = {
+            id,
+            aid: data.aid,
+            bvid: data.bvid,
+            cid: data.cid,
+            title: data.title,
+            artist: data.owner?.name,
+            artwork: data.pic.startsWith("//") ? "https:" + data.pic : data.pic
+        };
+
+        // 获取音频 URL
         const media = await core.getMediaSource(musicItem, quality);
 
         return {
@@ -25,11 +42,11 @@ module.exports = {
             artist: musicItem.artist,
             url: media.url,
             cover: musicItem.artwork,
-            lrc: "" // 你以后可以加歌词
+            lrc: "Nothing"
         };
     },
 
-    async getLyric(id) {
-        return { lrc: "" };
+    async getLyric() {
+        return { raw: "Nothing" };
     }
 };
